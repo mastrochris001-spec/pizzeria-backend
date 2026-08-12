da20f771f1839953f5444dca8a89ce3fac33d602
mastrochris001-spec <mastrochris001@gmail.com>
2026-08-12 13:21:11 +0200

    Update server.js

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-app.options('*', cors());
+app.options(/(.*)/, cors());
 
 app.use(helmet({
     contentSecurityPolicy: false
