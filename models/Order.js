3a3030b81fb59d45a347c5ed5e0a2d3871a1bac7
mastrochris001-spec <mastrochris001@gmail.com>
2026-09-04 11:53:36 +0200

    Update Order.js

--- models/Order.js
+++ models/Order.js
@@ -6,102 +6,53 @@ const OrderSchema = new mongoose.Schema({
         ref: 'User', 
         required: true 
     },
-
-    nomeClienteCustom: { 
-        type: String, 
-        default: "" 
-    },
-    
-    telefonoCliente: {
-        type: String,
-        required: true
-    },
-
+    nomeClienteCustom: { type: String, default: "" },
+    telefonoCliente: { type: String, required: true },
     pizze: [
         {
-            pizza: { 
-                type: mongoose.Schema.Types.ObjectId, 
-                ref: 'Pizza',
-                required: true
-            },
-            quantita: { 
-                type: Number, 
-                default: 1 
-            },
-            note: { 
-                type: String, 
-                default: "" 
-            }
+            pizza: { type: mongoose.Schema.Types.ObjectId, ref: 'Pizza', required: true },
+            quantita: { type: Number, default: 1 },
+            note: { type: String, default: "" }
         }
     ],
-    
-    totale: { 
-        type: Number, 
-        required: true 
-    },
-
-    puntiGuadagnati: {
-        type: Number,
-        default: 0
-    },
-
+    totale: { type: Number, required: true },
+    puntiGuadagnati: { type: Number, default: 0 },
     tipoOrdine: {
         type: String,
         enum: ['tavolo', 'asporto', 'consegna'],
         default: 'asporto'
     },
-
-    orario: { 
-        type: String, 
-        required: true 
-    },
-
-    caricoSlot: { 
-        type: Number, 
-        required: true 
-    },
-
+    orario: { type: String, required: true },
+    caricoSlot: { type: Number, required: true },
     metodoPagamento: {
         type: String,
-        enum: ['contanti', 'pos'],
+        enum: ['contanti', 'pos', 'carta_credito', 'prepagata', 'satispay'],
         default: 'contanti'
     },
-
     pagato: { type: Boolean, default: false },
-
     prontoForno: { type: Boolean, default: false },
     prontoCompositore: { type: Boolean, default: false },
-
-    // --- NUOVO: giorno di consegna/ritiro (YYYY-MM-DD) per ordini futuri ---
-    dataConsegna: {
-        type: String,
-        default: ''
-    },
-
-    indirizzoConsegna: {
-        type: String,
-        default: ""
-    },
-
-    citofono: {
-        type: String,
-        default: ""
-    },
-
-    noteConsegna: {
-        type: String,
-        default: ""
-    },
-
+    
+    // --- DATA CONSEGNA (per ordini futuri) ---
+    dataConsegna: { type: String, default: '' },
+    
+    indirizzoConsegna: { type: String, default: "" },
+    citofono: { type: String, default: "" },
+    noteConsegna: { type: String, default: "" },
     riderAssegnato: { 
         type: mongoose.Schema.Types.ObjectId, 
         ref: 'User',
         default: null 
     },
-    
     stato: { 
         type: String, 
         enum: ['in attesa', 'in preparazione', 'pronto', 'in consegna', 'consegnato', 'eliminato'], 
+        default: 'in attesa' 
+    },
+    createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Order', OrderSchema);        enum: ['in attesa', 'in preparazione', 'pronto', 'in consegna', 'consegnato', 'eliminato'], 
         default: 'in attesa' 
     },
     
