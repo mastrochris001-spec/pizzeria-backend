9c04cd7f799c16abeb7932b77c0b3a6e31647691
mastrochris001-spec <mastrochris001@gmail.com>
2026-09-11 01:17:02 +0200

    Update Reward.js

--- models/Reward.js
+++ models/Reward.js
@@ -4,12 +4,17 @@ const rewardSchema = new mongoose.Schema({
     nome: { type: String, required: true },
     descrizione: { type: String, default: '' },
     puntiRichiesti: { type: Number, required: true, min: 1 },
-    foto: { type: String, default: 'premio_default.png' },
-    tipo: { 
-        type: String, 
-        enum: ['pizza', 'bibita', 'dolce', 'sconto', 'altro'],
-        default: 'altro'
+    categoria: {
+        type: String,
+        enum: ['pizze classiche', 'pizze speciali', 'pizze maxi', 'calzoni', 'focacce ripiene', 'panozzi', 'hamburger', 'stuzzicherie', 'dolci', 'bibite'],
+        default: 'pizze classiche'
     },
+    prodottoId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Pizza',
+        default: null
+    },
+    foto: { type: String, default: 'premio_default.png' },
     attivo: { type: Boolean, default: true },
     ordine: { type: Number, default: 0 }
 }, { timestamps: true });
